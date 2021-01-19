f458553f004944c8eb90376824103b50e6b1bb92
John Forbis <John.Forbis@civicplus.com>
2021-01-18 20:57:04 -0600

    save point: finished starter code - ready to build out app; starting with routes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Dependencies
+import pandas as pd
 import numpy as np
 import datetime as dt
 
@@ -9,3 +10,22 @@ from sqlalchemy import create_engine, func, and_
 
 from flask import Flask, jsonify
 
+# Setting up connection
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
+
+# Database tables
+measurements = Base.classes.measurement
+stations = Base.classes.station 
+
+# App info
+app = Flask(__name__)
+
+# Routes
+# Home Page
+# Precipitation
+# Stations
+# Tobs
+# Start/End?
