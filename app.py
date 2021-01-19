0a984918ece50d7abb6daed265d6119dc2052ff9
jforbis <forbisjohn@gmail.com>
2021-01-18 23:46:53 -0600

    save point: I can get my first three routes to run but they don't run every single time I hit them. Can't figure out why. Stopping for the night.

--- app.py
+++ app.py
@@ -30,20 +30,19 @@ def welcome():
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/stationlist<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/1.0/start<br/>"
         f"/api/v1.0/start/end"
     )
 
-# Precipitation
+# Precipitation - Route Works
 # Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
 # Return the JSON representation of your dictionary.
-@app.route("/api/v1.0/precipitation/")
-
+@app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
-    results = session.query(measurements.date, measurements.prcp).order_by(measurements.date).all()
+    results = session.query(measurements.date, measurements.prcp).all()
 
     prcp_data = []
     for date, prcp in results:
@@ -55,23 +54,38 @@ def precipitation():
 
     return jsonify(prcp_data)
 
-# Stations
+# Station List - Route Works but not every time
 # Return a JSON list of stations from the dataset.
-@app.route("/api/v1.0/stations")
+@app.route("/api/v1.0/stationlist")
+def stationlist():
+    session = Session(engine)
+    results = session.query(stations.station, stations.name).all()
+    station_data = {}
+    for station, name in results:
+        station_data[station] = name
 
-# def stations():
-#     session = Session(engine)
-#     results = session.query(stations.id, stations.name).all()
-#     station_data = {}
-#     for id, name in results:
-#         station_data[id] = name
+    session.close()
+    return jsonify(station_data)
 
-#     session.close()
-#     return jsonify(station_data)
-print("Hello World")
-# Tobs
+# Tobs - Route Works but not every time
 # Query the dates and temperature observations of the most active station for the last year of data.
 # Return a JSON list of temperature observations (TOBS) for the previous year.
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+
+    recent_date = session.query(measurements.date).order_by(measurements.date.desc()).first()
+    recent_date_minus1year = (dt.datetime.strptime(recent_date[0],'%Y-%m-%d')-dt.timedelta(days=365)).strftime('%Y-%m-%d')
+
+    results = session.query(measurements.date, measurements.tobs).filter(measurements.date >= recent_date_minus1year).order_by(measurements.date).all()
+
+    date_data = []
+    for date,tobs in results:
+        dict_data = {}
+        dict_data[date] = tobs
+        date_data.append(dict_data)    
+    session.close()
+    return jsonify(date_data)
 
 # Start/End?
 # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
@@ -83,3 +97,6 @@ print("Hello World")
 # You will need to join the station and measurement tables for some of the queries.
 
 # Use Flask `jsonify` to convert your API data into a valid JSON response object.
+
+if __name__ == '__main__':
+    app.run(debug=True)
